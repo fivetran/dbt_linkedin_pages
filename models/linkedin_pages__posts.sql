7ffbb75afd298304a899b72af87db0cdfe064ddd
Dylan Baker <dylan@dbanalytics.co>
2021-10-20 18:06:22 -0400

    initial models + config

---
+++ models/linkedin_pages__posts.sql
@@ -0,0 +1,52 @@
+with share_statistic as (
+
+    select *
+    from {{ var('share_statistic') }}
+
+),
+
+ugc_post_share_statistic as (
+
+    select *
+    from {{ var('ugc_post_share_statistic') }}
+
+),
+
+ugc_post_history as (
+
+    select *
+    from {{ var('ugc_post_history') }}
+
+),
+
+ugc_post_share_content_media as (
+
+    select *
+    from {{ var('ugc_post_share_content_media') }}
+
+),
+
+joined as (
+
+    select 
+        share_statistic.click_count,
+        ugc_post_share_statistic.ugc_post_id,
+        ugc_post_history.post_author,
+        ugc_post_history.created_timestamp,
+        ugc_post_history.first_published_timestamp,
+        ugc_post_history.lifecycle_state,
+        ugc_post_history.version_tag,
+        ugc_post_share_content_media.title_text,
+        ugc_post_share_content_media.original_url,
+    from share_statistic
+    left join ugc_post_share_statistic
+        on share_statistic.share_statistic_id = ugc_post_share_statistic.share_statistic_id
+    left join ugc_post_history
+        on ugc_post_share_statistic.ugc_post_id = ugc_post_history.ugc_post_id
+    left join ugc_post_share_content_media
+        on ugc_post_history.ugc_post_id = ugc_post_share_content_media.ugc_post_id
+
+)
+
+select *
+from joined
